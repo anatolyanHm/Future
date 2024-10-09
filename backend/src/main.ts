564a7f999cd5fa80f0c2eaea9dee65fe823180d1
Your Name <you@example.com>
2024-10-09 20:28:09 +0200

    maitts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
 
   app.setGlobalPrefix('/api');
   app.enableCors({
-    origin: true,
+    origin: '*',
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());
